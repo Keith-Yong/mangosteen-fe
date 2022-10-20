28a6ad54846c081d78e55ba79830c853e52427e1
keith-Yong <2957831617@qq.com>
2022-10-20 17:27:12 +0800

    template语法的vue3

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,12 @@ import { ref } from 'vue';
 
 //  ref（）的作用官方文档
 // https://cn.vuejs.org/guide/essentials/reactivity-fundamentals.html#ref-unwrapping-in-templates
+
 const count = ref(0)
 const add1 = () => {
-  
-  count.value += 1
+  console.log('1')
+  count.value += 1;
+
 }
 </script>
 
@@ -23,6 +25,7 @@ const add1 = () => {
     <!-- 当 ref 在模板中作为顶层属性被访问时，它们会被自动“解包”，所以不需要使用 .value -->
     
   <div>
+    {{count}}
     
   </div>
 </template>
