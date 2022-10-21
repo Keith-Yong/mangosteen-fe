651620b427cf56e3bfab4db28b69b52363983ecc
keith-Yong <2957831617@qq.com>
2022-10-21 15:19:30 +0800

    Bug描述：引入router失败 ，预期结果页面展示FOO文本

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
 import { App } from './App'
 import { Foo } from './views/Foo'
 import { Bar } from './views/Bar'
