61c258439e26119593e8dfd52eb074ddafd30999
keith-Yong <2957831617@qq.com>
2022-10-22 16:11:05 +0800

    bug:html和body的高度不一致

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,14 @@ import {RouterView}  from "vue-router"
 // 导出
 export const App = defineComponent({
  setup(){
-  return () => <>
-   <header>导航</header>
-  <div>
-      {/* router-link路由跳转 */}
-      <ul>
-         <li>
-         <router-link to="/">Foo</router-link>
-         </li>
-         <li>
-         <router-link to="/about">Bar</router-link>
-         </li>
-      </ul>
-      
-  
+  return () => 
+  (
+   <div>
       <RouterView/>
   </div>
-  <footer>页脚</footer>
-  </>
+  )
+  
+  
  }
 
 
--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,11 +1,21 @@
 import { defineComponent } from "vue";
 import {RouterView} from 'vue-router'
+import s from './Welcome.module.scss'
+import Logo from '../assets/icons/dolphin.svg'
+// console.log(Logo)
 //  定义欢迎页的主页
 export const Welcome = defineComponent({
     setup:(props,context) => {
-        return () => (
+        // s.wrapper为什么会对所有路由的样式都有影响？
+        return () => <div class={s.wrapper}>
        
-        <div><RouterView/></div>
-        )
+        <header>
+            <img src={Logo}  />
+            <h1>海豚记账</h1>
+        </header>
+        <main><RouterView/></main>
+        
+        
+        </div>
     }
 })
