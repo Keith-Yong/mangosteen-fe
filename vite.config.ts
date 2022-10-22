808823687825116941f452641f8bf53c7b1ef289
keith-Yong <2957831617@qq.com>
2022-10-22 16:33:44 +0800

    git可以预览

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   // base:'/mangosteen-fe/dist/',
+  base:'/',
   plugins: [
     vue(),
     vueJsx({
