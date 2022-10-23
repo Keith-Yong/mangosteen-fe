ccd52d566bc630981da794db47ef120ffd203720
keith-Yong <2957831617@qq.com>
2022-10-23 19:54:39 +0800

    下一页，跳过按钮的位置处理

--- src/components/welcome/first.tsx
+++ src/components/welcome/first.tsx
@@ -1,17 +1,23 @@
 import { defineComponent } from "vue";
 import s from './First.module.scss'
 import pig from '../../assets/icons/Piggybank.svg'
+import {RouterLink} from 'vue-router'
 export const First = defineComponent({
     setup: (props, context) => {
       return () => (
         <div class={s.wrapper}>
           <div class={s.card}>
-            <img src={pig}/>
+            <img class={s.pig} src={pig}/>
 
           </div>
 
           <div class={s.actions}>
-
+          {/* 在这里定义底部的两个按钮路由标签 */}
+          {/* 如何完成下一页在中间，跳过在左边：做两个跳过，左边的隐藏处理 */}
+          <RouterLink class={s.fake} to="start">跳过</RouterLink>
+          <RouterLink to="welcome/2">下一页</RouterLink>
+          <RouterLink to="start">跳过</RouterLink>
+          
           </div>
         </div>
       )
