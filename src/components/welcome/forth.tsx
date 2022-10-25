6c4e15dfc8e7397f7339b7ef890bd1dbbfc28e8a
keith-Yong <2957831617@qq.com>
2022-10-25 23:26:56 +0800

    重构：提取 WelcomeLayout 组件

--- src/components/welcome/forth.tsx
+++ src/components/welcome/forth.tsx
@@ -2,24 +2,22 @@ import { defineComponent } from "vue";
 import s from './First.module.scss'
 import cloud from '../../assets/icons/cloud.svg'
 import {RouterLink} from 'vue-router'
+import { WelcomeLayout } from './WelcomeLayout'; 
 export const Forth = defineComponent({
     setup: (props, context) => {
       return () => (
-        <div class={s.wrapper}>
-          <div class={s.card}>
-           <img src={cloud} />
-            <h2>云存储<br />再也不用担心数据丢失</h2>
-          </div>
+        <WelcomeLayout>
+        {{
+          icon: () => <img class={s.icon} src={cloud} />,
+          title: () => <h2>每日提醒<br />不遗漏每一笔账单</h2>,
+          buttons: () => <>
+            <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
+            <RouterLink to="/start" >完成</RouterLink>
+            <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
+          </>
 
-          <div class={s.actions}>
-          {/* 在这里定义底部的两个按钮路由标签 */}
-          {/* 如何完成下一页在中间，跳过在左边：做两个跳过，左边的隐藏处理 */}
-          <RouterLink class={s.fake} to="/start">跳过</RouterLink>
-          <RouterLink to="/start" >完成</RouterLink>
-          <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
-          
-          </div>
-        </div>
+        }}
+      </WelcomeLayout>
       )
     }
   })
--- src/components/welcome/third.tsx
+++ src/components/welcome/third.tsx
@@ -1,25 +1,22 @@
 import { defineComponent } from "vue";
 import s from './First.module.scss'
 import chart from '../../assets/icons/chart.svg'
+import { WelcomeLayout } from './WelcomeLayout'
 import {RouterLink} from 'vue-router'
 export const Third = defineComponent({
     setup: (props, context) => {
       return () => (
-        <div class={s.wrapper}>
-          <div class={s.card}>
-           <img class={s.icon} src={chart}  />
-            <h2>数据可视化<br />收支一目了然</h2>
-          </div>
-
-          <div class={s.actions}>
-          {/* 在这里定义底部的两个按钮路由标签 */}
-          {/* 如何完成下一页在中间，跳过在左边：做两个跳过，左边的隐藏处理 */}
-          <RouterLink class={s.fake} to="/start">跳过</RouterLink>
-          <RouterLink to="/welcome/4">下一页</RouterLink>
-          <RouterLink to="/start">跳过</RouterLink>
-          
-          </div>
-        </div>
+       <WelcomeLayout>
+        {{
+          icon:() =>  <img class={s.icon} src={chart} />,
+          title:() =>  <h2>每日提醒<br/>不遗漏每一笔账单</h2>,
+          buttons:() => <>
+          <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
+          <RouterLink to="/welcome/4" >下一页</RouterLink>
+          <RouterLink to="/start" >跳过</RouterLink>
+          </>
+        }}
+       </WelcomeLayout>
       )
     }
   })
