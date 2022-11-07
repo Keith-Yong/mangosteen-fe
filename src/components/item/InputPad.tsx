1a349237e74d70ae2fbf8e3e7780c20481f0766e
keith-Yong <2957831617@qq.com>
2022-11-07 16:48:24 +0800

    完成键盘数字的输入功能

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -16,7 +16,36 @@ export const InputPad =defineComponent({
         const now = new Date()//？？??
         const refDate = ref<Date>(now) //？？??
         //  appendText函数传入数字或字符串都转化为字符串
-        const appendText = (n:number | string) => refAmount.value += n.toString()
+        // const appendText = (n:number | string) => refAmount.value += n.toString()
+        const appendText = (n:number | string) => {
+            const nString = n.toString() //转换为字符串
+            const  doIndex = refAmount.value.indexOf('.') // 获取键盘上的数字中.的下标
+            if (refAmount.value.length >= 13) { //长度最多13位
+                return
+            }
+            //键盘上的数字存在符号点. ，且键盘上的数字的长度？？??怎么理解这个条件
+            if (doIndex >= 0 && refAmount.value.length -doIndex >2){ 
+                return
+            }
+            // 如果传入的参数是小数点，
+            if (nString === '.') {
+                if (doIndex >= 0) { // 已经有小数点了，则终止当前判断语句执行
+                    return
+                  }
+                   // 如果输入键盘的是0，
+            } else if  (nString === '0') {
+                if (doIndex === -1){ //没有小数点
+                    if(refAmount.value === '0'){ //但是有0，则不执行
+                        return
+                    }
+                }
+            } else {  
+                if(refAmount.value === '0') {// 如果键盘显示的数值的是0，
+                    refAmount.value = ''// 则置空，
+                }
+            }
+            refAmount.value += n.toString() //键盘显示的数值是所有输入数值的拼接
+        }
         
         //  按键上的按钮,点击后调用appendText函数
         const buttons = [
@@ -31,7 +60,7 @@ export const InputPad =defineComponent({
             {text:'9',onClick: () => {appendText(9)}},
             {text:'.',onClick: () => {appendText('.')}},
             {text:'0',onClick: () => { appendText(0)}},
-            {text:'清空', onClick: () => { } },
+            {text:'清空', onClick: () => {refAmount.value = '0' } }, //清空即置0
             {text:'提交', onClick: () => { } },
             
         ]
@@ -40,7 +69,7 @@ export const InputPad =defineComponent({
         const showDatePicker = () => refDatePickerVisible.value = true
         const hideDatePicker = () => refDatePickerVisible.value = false
         const setDate = (date: Date) => { refDate.value = date; hideDatePicker() }
-        const refAmount = ref('') //键盘上显示的数字初始值为空字符串
+        const refAmount = ref('0') //键盘上显示的数字初始值为0
         return () => 
         <>
             <div  class={s.dateAndAmount}>
