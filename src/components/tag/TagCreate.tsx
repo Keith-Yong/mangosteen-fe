436b6562d1d274c650cb6533c76e29564b6f2e23
keith-Yong <2957831617@qq.com>
2022-11-15 15:47:10 +0800

    创建tagedit组件实现标签确定页面

--- src/components/tag/TagCreate.tsx
+++ src/components/tag/TagCreate.tsx
@@ -2,15 +2,18 @@ import { Button } from '../../shared/Button';
 import { defineComponent, PropType, reactive, toRaw, ref } from 'vue';
 import { MainLayout } from '../../layouts/MainLayout';
 import { Icon } from '../../shared/Icon';
-import s from './TagCreate.module.scss'
+// import s from './TagCreate.module.scss'
 import { EmojiSelect } from '../../shared/EmojiSelect';
 import { Rules, validate } from '../../shared/validate';
+import s from './Tag.module.scss'
+
+
 export const TagCreate = defineComponent({
-  props: {
-    name: {
-      type: String as PropType<string>
-    }
-  },
+  // props: {
+  //   name: {
+  //     type: String as PropType<string>
+  //   }
+  // },
   setup: (props, context) => {
     const formData = reactive({ //reactive包裹的对象成为响应式数据的对象
       name: '', //name输入的内容，sign指点击的内容初始化为空值
