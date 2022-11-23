48e2d4d00b8cc363d2390fcdd33bf6379b41c863
keith-Yong <2957831617@qq.com>
2022-11-23 15:53:23 +0800

    完成第三个图标功能及样式

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -1,4 +1,4 @@
-import { defineComponent, onMounted, PropType, ref } from "vue";
+import { computed, defineComponent, onMounted, PropType, reactive, ref } from "vue";
 import { FormItem } from "../../shared/Form";
 import s from './Charts.module.scss'
 import * as echarts from 'echarts';
@@ -22,6 +22,23 @@ export const Charts = defineComponent({
         // 获取元素作为响应式变量
         const refDiv = ref<HTMLDivElement>()
         const refDiv2 = ref<HTMLDivElement>()
+        //定义图标的内容，响应变量，数组内嵌套对象
+        const data3 = reactive( [
+            {tag: {id:1, name:'房租', sign:'x'}, amount:3000},
+            {tag: {id:2, name:'吃饭房租', sign:'x'}, amount:1000},
+            {tag: {id:3, name:'娱乐', sign:'x'}, amount:900},
+        ])
+
+        //使用计算属性对data3进行计算返回新的数组  为什么
+        const betterData3 = computed ( () => {
+            //reduce函数用于计算数组中所有值的总和
+            const total = data3.reduce( (sum,item) => sum + item.amount,0)
+
+            return data3.map( item => ( {
+             ...item, //解构获取item的值  为什么
+             percent: Math.round(item.amount / total * 100) + '%'
+            }))
+        })
         onMounted( () => {
             if (refDiv.value === undefined) {return}
             // 初始化eharts对象
@@ -91,6 +108,28 @@ export const Charts = defineComponent({
                 ]} v-model={category.value} />
                 <div ref={refDiv} class={s.demo}></div>
                 <div ref={refDiv2} class={s.demo2}></div>
+                <div class={s.demo3}>
+                    {betterData3.value.map( ( {tag,amount,percent} ) => { //为什么 {tag,amount,percent}这个对象是如何拿到的 
+                        return (
+                            <div class={s.topItem}>
+                                 <div class={s.sign}>
+                                    {tag.sign}
+                                 </div>
+                                 <div class={s.bar_wrapper}>
+                                    <div class={s.bar_text}>
+                                        <span>{tag.name} - {percent}</span>
+                                        <span>￥ {amount} </span>
+                                    </div>
+                                    <div class={s.bar}>
+                                        <div class={s.bar_inner}></div>
+                                    </div>
+                                    
+                                 </div>
+                            </div>
+                        )
+                    }
+                    )}
+                </div>
             </div>
         )
     }
