d219f66d6f09f5c66a51f5d49bc791534846ae01
keith-Yong <2957831617@qq.com>
2023-01-05 18:50:53 +0800

    触发长按效果

--- src/components/item/Tags.tsx
+++ src/components/item/Tags.tsx
@@ -1,10 +1,11 @@
 import { Icon } from '../../shared/Icon';
-import { defineComponent, PropType } from "vue";
+import { defineComponent, PropType, ref } from "vue";
 import { http } from "../../shared/Http";
 import { useTags } from "../../shared/useTags";
 import { Button } from '../../shared/Button';
 import s from './Tags.module.scss';
 import { RouterLink } from 'vue-router';
+import { time } from 'echarts';
 
 export const Tags = defineComponent({
     props: {
@@ -26,37 +27,70 @@ export const Tags = defineComponent({
 
         const onSelect = (tag: Tag) => {
           context.emit('update:selected', tag.id)
+        };
+
+        const timer = ref<number>()
+        const currentTag = ref<HTMLDivElement>()
+
+        const onLongPress = () => {
+          console.log('长按')
         }
 
-        return () => <>
-        <div class={s.tags_wrapper}>
-        {/* 把kind属性添加到路径的查询参数上拼接 */}
-        <RouterLink to={`/tags/create?kind=${props.kind}`} class={s.tag}>
-          <div class={s.sign}>
-            <Icon name="add" class={s.createTag} />
-          </div>
-          <div class={s.name}>新增</div>
-          </RouterLink>
-        
-        {tags.value.map(tag =>
-             <div class={[s.tag, props.selected === tag.id ? s.selected : '']}
-            onClick={() => onSelect(tag)}
-          >
-            <div class={s.sign}>
-              {tag.sign}
-            </div>
-            <div class={s.name}>
-              {tag.name}
-            </div>
-          </div>
-        )}
-      </div>
-      <div class={s.more}>
-        {hasMore.value ?
-          <Button class={s.loadMore} onClick={fetchTags}>加载更多</Button> :
-          <span class={s.noMore}>没有更多</span>
+        // 触发时开始计时
+        const onTouchStart = (e:TouchEvent) => {
+          currentTag.value = e.currentTarget as HTMLDivElement
+          timer.value = setTimeout(() => {
+            onLongPress()
+          }, 500);
         }
-      </div>
+
+        const onTouchEnd = (e:TouchEvent) => {
+          clearTimeout(timer.value)
+        }
+
+        //获取e.target移动后没有变化，用document.elementFromPoint方法，传touches的clientX和Y可以获取到点击时的元素，并且移动时也能获取到改变的元素
+        const onTouchMove = (e:TouchEvent) => {
+          const pointedElement = document.elementFromPoint(e.touches[0].clientX,e.touches[0].clientY)
+          if(currentTag.value !== pointedElement && 
+            currentTag.value?.contains(pointedElement) === false){
+              clearTimeout(timer.value)
+            }
+        }
+
+
+
+        return () => <>
+        {/*  为什么要在这里添加一个Div，再触发函数事件 */}
+        <div class={s.tags_wrapper}  onTouchmove={onTouchMove}>
+        {/* 把kind属性添加到路径的查询参数上拼接 */}
+          <RouterLink to={`/tags/create?kind=${props.kind}`} class={s.tag}>
+            <div class={s.sign}>
+              <Icon name="add" class={s.createTag} />
+            </div>
+            <div class={s.name}>新增</div>
+            </RouterLink>
+          
+          {tags.value.map(tag =>
+              <div class={[s.tag, props.selected === tag.id ? s.selected : '']}
+              onClick={() => onSelect(tag)}
+              onTouchstart={onTouchStart}
+              onTouchend={onTouchEnd}
+            >
+              <div class={s.sign}>
+                {tag.sign}
+              </div>
+              <div class={s.name}>
+                {tag.name}
+              </div>
+            </div>
+          )}
+        </div>
+        <div class={s.more}>
+          {hasMore.value ?
+            <Button class={s.loadMore} onClick={fetchTags}>加载更多</Button> :
+            <span class={s.noMore}>没有更多</span>
+          }
+        </div>
         </>
     }
 })
