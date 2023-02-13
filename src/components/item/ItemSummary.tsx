b62c3a51f04069b4af239bf50fd6afbbf13f5bb0
keith-Yong <2957831617@qq.com>
2023-02-13 19:46:27 +0800

    封装 useItemStore

--- src/components/item/ItemSummary.tsx
+++ src/components/item/ItemSummary.tsx
@@ -9,6 +9,7 @@ import { FloatButton } from "../../shared/FloatButton";
 import { http } from "../../shared/Http";
 import { Icon } from "../../shared/Icon";
 import { Money } from "../../shared/Money";
+import { useItemStore } from "../../stores/useItemStore";
 import s from './ItemSummary.module.scss'
 
 export const  ItemSummary = defineComponent({
@@ -23,59 +24,59 @@ export const  ItemSummary = defineComponent({
           }
     },
     setup: (props, context) => {
-      const items = ref<Item[]>([])
-      const hasMore = ref(false)
-      const page = ref(0)
-      const fetchItems = async () => {
-        if(!props.startDate || !props.endDate){ return }
-        const response = await http.get<Resources<Item>>('/items', {
-          happen_after: props.startDate,
-          happen_before: props.endDate,
-          page: page.value + 1,
-        }, {
-          _mock: 'itemIndex',
-          _autoLoading: true,
-        })
-        const { resources, pager } = response.data
-        items.value?.push(...resources)
-        hasMore.value = (pager.page - 1) * pager.per_page + resources.length < pager.count
-        page.value += 1
+      if (!props.startDate || !props.endDate) {
+        return () => <div>请先选择时间范围</div>
       }
-      useAfterMe(fetchItems)
+      const itemStore = useItemStore(['items', props.startDate, props.endDate])
+    useAfterMe(() => itemStore.fetchItems(props.startDate, props.endDate))
 
-      watch(()=>[props.startDate,props.endDate], ()=>{
-        items.value = []
-        hasMore.value = false
-        page.value = 0
-        fetchItems()
-      })
+    watch(
+      () => [props.startDate, props.endDate],
+      () => {
+        itemStore.reset()
+        itemStore.fetchItems()
+      }
+    )
+
 
       const itemsBalance = reactive({
         expenses: 0, income: 0, balance: 0
       })
       // onMounted(async ()=>{
-        const fetchItemsBalance =async ()=>{
-        if(!props.startDate || !props.endDate){ return }
-        const response = await http.get('/items/balance', {
-          happen_after: props.startDate,
-          happen_before: props.endDate,
-          page: page.value + 1,
-        }, {
-          _mock: 'itemIndexBalance',
-        })
+      const fetchItemsBalance = async () => {
+          if (!props.startDate || !props.endDate) {
+            return
+          }
+          const response = await http.get(
+            '/items/balance',
+            {
+              happen_after: props.startDate,
+              happen_before: props.endDate
+            },
+            {
+              _mock: 'itemIndexBalance'
+            }
+          )
+
         Object.assign(itemsBalance, response.data)
       }
       useAfterMe(fetchItemsBalance)
-      watch(()=>[props.startDate,props.endDate], ()=>{
-        Object.assign(itemsBalance, {
-          expenses: 0, income: 0, balance: 0
-        })
-        fetchItemsBalance()
-      })
+     
+      watch(
+        () => [props.startDate, props.endDate],
+        () => {
+          Object.assign(itemsBalance, {
+            expenses: 0,
+            income: 0,
+            balance: 0
+          })
+          fetchItemsBalance()
+        }
+      )
 
         return () => (
             <div class={s.wrapper}>
-           { (items.value && items.value.length > 0) ? (
+             {itemStore.items && itemStore.items.length > 0 ? (
           <>
             <ul class={s.total}>
               <li>
@@ -93,7 +94,7 @@ export const  ItemSummary = defineComponent({
               </li>
             </ul>
             <ol class={s.list}>
-              {items.value.map((item) => (
+            {itemStore.items.map((item)=> (
                 <li>
                   <div class={s.sign}>
                     
@@ -101,19 +102,25 @@ export const  ItemSummary = defineComponent({
                   </div>
                   <div class={s.text}>
                     <div class={s.tagAndAmount}>
-                    <span class={s.tag}>{ item.tags && item.tags.length > 0 ? item.tags[0].name : '未分类'}</span>
-                      <span class={s.amount}>￥<Money value={item.amount}/></span>
+                    <span class={s.tag}>{item.tags && item.tags.length > 0 ? item.tags[0].name : '未分类'}</span>
+                      <span class={s.amount}>
+                        ￥<Money value={item.amount} />
+                      </span>
                     </div>
-                    <div class={s.time}><Datetime value={item.happen_at}/></div>
+                    <div class={s.time}>
+                      <Datetime value={item.happen_at} />
+                    </div>
+                   
                   </div>
                 </li>
               ))}
             </ol>
             <div class={s.more}>
-              {hasMore.value ?
-                <Button onClick={fetchItems}>加载更多</Button> :
+            {itemStore.hasMore ? (
+                <Button onClick={() => itemStore.fetchItems(props.startDate, props.endDate)}>加载更多</Button>
+              ) : (
                 <span>没有更多</span>
-              }
+                )}
             </div>
             </>
               ) : (
