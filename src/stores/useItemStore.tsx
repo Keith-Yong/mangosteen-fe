af411feed259e8377cdaabe74736b1fdea0b4d52
keith-Yong <2957831617@qq.com>
2023-02-13 19:54:24 +0800

    修复错误地请求了下一页 items 的 bug

--- src/stores/useItemStore.tsx
+++ src/stores/useItemStore.tsx
@@ -9,10 +9,12 @@ type State = {
 
   type Actions = {
     reset: () => void
+    _fetch: (firstPage: boolean, startDate?: string, endDate?: string) => void
     fetchItems: (startDate?: string, endDate?: string) => void
+    fetchNextPage: (startDate?: string, endDate?: string) => void
   }
 
-  
+
   export const useItemStore = (id: string | string[]) => defineStore<string, State, {}, Actions>(
     typeof id === 'string' ? id : id.join('-'), {
         state: () => ({
@@ -26,7 +28,7 @@ type State = {
             this.hasMore = false
             this.page = 0
           },
-          async fetchItems(startDate, endDate) {
+          async _fetch(firstPage, startDate, endDate) {
             if (!startDate || !endDate) {
               return
             }
@@ -35,7 +37,7 @@ type State = {
               {
                 happen_after: startDate,
                 happen_before: endDate,
-                page: this.page + 1
+                page: firstPage ? 1 : this.page + 1
               },
               {
                 _mock: 'itemIndex',
@@ -43,9 +45,19 @@ type State = {
               }
             )
             const { resources, pager } = response.data
-            this.items?.push(...resources)
+            if (firstPage) {
+                this.items = resources
+              } else {
+                this.items.push(...resources)
+              }
             this.hasMore = (pager.page - 1) * pager.per_page + resources.length < pager.count
             this.page += 1
+        },
+        async fetchNextPage(startDate, endDate) {
+          this._fetch(false, startDate, endDate)
+        },
+        async fetchItems(startDate, endDate) {
+          this._fetch(true, startDate, endDate)
           }
         }
       }
